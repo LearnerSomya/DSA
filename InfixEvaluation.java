74d336e13639a8dfc7fd2f99c78416170b01e256
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-08 01:24:29 +0530

    Infix Evaluation

--- InfixEvaluation.java
+++ InfixEvaluation.java
@@ -13,8 +13,58 @@ class InfixEvaluation {
             } else if(Character.isDigit(ch)){
                 operands.push(ch - '0');
             } else if( ch == '+' || ch == '-' || ch == '*' || ch == '/'){
-                while(operators.size){}
+                while(operators.size() > 0 && operators.peek() != 'c' && precedence(ch) <= precedence(operators.peek())){
+                    int val2 = operands.pop();
+                    int val1 = operands.pop();
+                    char opr = operators.pop();
+                    int oprval = operations(val1, val2, opr);
+                    operands.push(oprval);
+                }
+                operators.push(ch);
+            } else if(ch == ')'){
+                while(operators.size() > 0 && operators.peek() != '('){
+                    int val2 = operands.pop();
+                    int val1 = operands.pop();
+                    int oprval = operations(val1, val2, opr);
+                    operands.push(oprval);
+                }
+                if(operators.size() > 0){
+                    operators.pop();
+                }
             }
+        }
+
+        while(operators.size() > 0){
+            int val2 = operands.pop();
+            int val1 = operands.pop();
+            char oprval = operations(val1, val2, opr);
+            operands.push(oprval);
+        }
+        int val = operands.pop();
+        System.out.println(val);
+    }
+
+    public static int precedence(char op){
+        if(op == '+'){
+            return 1;
+        } else if(op == '-'){
+            return 1;
+        } else if(op == '*'){
+            return 2;
+        } else if(op == '/'){
+            return 2;
+        }
+    }
+
+    public static int operations(int val1, int val2, char operator){
+        if(operator == '+'){
+            return val1 + val2;
+        } else if(operator == '-'){
+            return val1 - val2;
+        } else if(operator == '*'){
+            return val1 * val2;
+        } else if(operator == '/'){
+            return val1 / val2;
         }
     }
 }
