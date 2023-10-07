886f601e51031ace5bbd568552ce4c666b61c163
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-07 23:44:42 +0530

    Infix Evaluation

---
+++ InfixEvaluation.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class InfixEvaluation {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String Expression = scn.next();
+        Stack <Integer> operands = new Stack<>();
+        Stack <Integer> operators = new Stack<>();
+        for(int i = 0; i < Expression.length(); i++){
+            char ch = Expression.charAt(i);
+            
+        }
+    }
+}
