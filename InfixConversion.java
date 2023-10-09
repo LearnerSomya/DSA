60f2b813dbed2277a01f03e61f435087e5aa707f
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-09 23:48:04 +0530

    Infix Conversion into Postfix and Prefix

---
+++ InfixConversion.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class InfixConversion {
+    public static void main(String[] args) throws Exception{
+        BufferedReader brVal = new BufferedReader(new InputStreamReader(System.in));
+        String Expression = brVal.readLine();
+        Stack <Integer> Prefix = new Stack<>();
+        Stack <Character> Operators = new Stack<>();
+        Stack <Integer> Postfix = new Stack<>();
+    }
+}
