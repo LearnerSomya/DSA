c01671da6e39af432049b0cd4fa201125d849e91
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-14 23:53:29 +0530

    Infix Conversion

--- InfixConversion.java
+++ InfixConversion.java
@@ -4,8 +4,30 @@ class InfixConversion {
     public static void main(String[] args) throws Exception{
         BufferedReader brVal = new BufferedReader(new InputStreamReader(System.in));
         String Expression = brVal.readLine();
-        Stack <Integer> Prefix = new Stack<>();
+        Stack <String> Prefix = new Stack<>();
         Stack <Character> Operators = new Stack<>();
-        Stack <Integer> Postfix = new Stack<>();
+        Stack <String> Postfix = new Stack<>();
+        for (int i = 0; i < Expression.length(); i++){
+            char ch = Expression.charAt(i);
+            if(ch == '('){
+                Operators.push(ch);
+            } else if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <. 'z') || (ch >= 'A' && ch <= 'Z' )){
+
+            } else if (ch == '+' || ch == '-' || ch == '/' || ch == '*'){
+
+            } else if(ch == ')'){
+
+            }
+        }
+    }
+
+    public static int Precedence(char op){
+        if(op == '+' || op == '-'){
+            return 1;
+        } else if (op == '*' || op == '/'){
+            return 2;
+        } else {
+            return 0;
+        }
     }
 }
