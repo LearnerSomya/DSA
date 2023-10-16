90c3f4dba7468760844bd111e46db2b31b921f1c
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-16 23:58:45 +0530

    Postfix

---
+++ PostfixEvaluationNConversion.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class PostfixEvaluationNConversion {
+    public static void main(String [] args) throws Exception{
+        BufferedReader brVal = new BufferedReader(new InputStreamReader(System.in));
+        String Expression = brVal.readLine();
+
+        
+    }
+}
+
