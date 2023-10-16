823847812ada2f637a12412f6a9a37b61351c185
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-16 23:53:24 +0530

    Postfix Evaluation and Conversion

---
+++ PostfixEvaluationConversion.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class PostfixEvaluationConversion {
+    public static void main(String [] args) throws Exception{
+        BufferedReader brVal = new BufferedReader(new InputStreamReader(System.in));
+        String Expression = brVal.readLine();
+
+        
+    }
+}
