bcde72d30621e67d93bc47d4ba4a542a95ef8c6c
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-20 23:34:13 +0530

    Merge Overlapping

---
+++ MergeOverlapping.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class MergeOverlapping {
+    public static void main(String [] args) throws Exception{
+        BufferedReader brVal = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter count of intervals you want to enter");
+        int num = Integer.parseInt(brVal.readLine());
+    }
+}
