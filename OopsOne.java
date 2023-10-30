cb9386dbb0b19179439a47d7d4d8e53adf67dae6
LearnerSomya <somyasinghal71001@gmail.com>
2023-10-30 22:42:09 +0530

    OOPs

--- OopsOne.java
+++ OopsOne.java
@@ -2,7 +2,15 @@ import java.io.*;
 import java.util.*;
 class OopsOne {
     public static void main(String[] args){
+        Person p1 = new Person();
+        p1.age = 10;
+        p1.name = "A";
+        p1.saysHi();
 
+        Person p2 = new Person();
+        p2.age = 20;
+        p2.name = "B";
+        p2.saysHi();
     }
 
     //Here we created class Person
