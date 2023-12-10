6294fcb987c4f9ed6d1798a27f30008497307dc5
LearnerSomya <somyasinghal71001@gmail.com>
2023-12-10 23:55:33 +0530

    Build Normal Stack

--- BuildNormalStack.java
+++ BuildNormalStack.java
@@ -1,5 +1,17 @@
 import java.io.*;
 import java.util.*;
 class BuildNormalStack {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public static class CustomStack{
+        int[] data;
+        int tos;
+
+        public CustomStack(int cap){
+            data = new int[cap];
+            tos = -1;
+        }
+    }
 }
