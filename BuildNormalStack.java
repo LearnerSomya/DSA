9eb54c5715b33a0cd5ce8a1a051982cf94992045
LearnerSomya <somyasinghal71001@gmail.com>
2023-12-11 23:55:19 +0530

    Build Normal Stack

--- BuildNormalStack.java
+++ BuildNormalStack.java
@@ -1,8 +1,10 @@
 import java.io.*;
 import java.util.*;
 class BuildNormalStack {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)throws Exception{
+        BufferedReader brVal = new BufferedReader (new InputStreamReader(System.in));
+        int n = Integer.parseInt(brVal.readLine());
+        CustomStack st = new CustomStack(n);
     }
 
     public static class CustomStack{
@@ -26,5 +28,34 @@ class BuildNormalStack {
             }
             System.out.println();
         }
+
+        void push(int val){
+            if(tos == data.length - 1){
+                System.out.println("Stack Overflow");
+            } else {
+                tos++;
+                data[tos] = val;
+            }
+        }
+
+        int pop(){
+            if(tos == -1){
+                System.out.println("Stack nUnderflow");
+                return -1;
+            } else{
+                 int val = data[tos];
+                 tos--;
+                 return val;
+            }
+        }
+        int top(){
+            if(tos == -1){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else{
+                return data[tos];
+            }
+        }
+
     }
 }
