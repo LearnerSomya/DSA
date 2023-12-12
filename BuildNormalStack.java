c66d6e7d723bddf59b6f9b3d890983753a010c76
LearnerSomya <somyasinghal71001@gmail.com>
2023-12-12 22:48:23 +0530

    Build Normal Stack

--- BuildNormalStack.java
+++ BuildNormalStack.java
@@ -3,14 +3,36 @@ import java.util.*;
 class BuildNormalStack {
     public static void main(String[] args)throws Exception{
         BufferedReader brVal = new BufferedReader (new InputStreamReader(System.in));
+        //System.out.println("enter size of the stack");
         int n = Integer.parseInt(brVal.readLine());
         CustomStack st = new CustomStack(n);
+        String str = brVal.readLine();
+        while(str.equals("quit") == false){
+            if(str.startsWith("push")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                st.push(val);
+            } else if(str.startsWith("pop")){
+                int val = st.pop();
+                if(val != -1){
+                    System.out.println(val);
+                } 
+            } else if(str.startsWith("top")){
+                int val = st.top();
+                if(val != -1){
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")){
+                System.out.println(st.size());
+            } else if(str.startsWith("display")){
+                st.display();
+            } str = brVal.readLine();
+        }
     }
 
     public static class CustomStack{
+        //System.out.println("Eneter values in stack");
         int[] data;
         int tos;
-
         //Top Of Stack
         public CustomStack(int cap){
             data = new int[cap];
