4b8b41d72e767a1f68c95071b17823b9a8586a11
LearnerSomya <somyasinghal71001@gmail.com>
2023-12-14 23:56:50 +0530

    Build Dynamic Stack

--- BuildDynamicStack.java
+++ BuildDynamicStack.java
@@ -31,6 +31,53 @@ class BuildDynamicStack{
         }
     }
     public static class DynamicStack{
+        //System.out.println("Eneter values in stack");
+        int[] data;
+        int tos;
+        //Top Of Stack
+        public CustomStack(int cap){
+            data = new int[cap];
+            tos = -1;
+        }
 
+        //Size of the stack
+        int size(){
+            return tos + 1;
+        }
+
+        void display(){
+            for(int i = tos; i >= 0; i--){
+                System.out.print(data[i] + " ");
+            }
+            System.out.println();
+        }
+
+        void push(int val){
+            if(tos == data.length - 1){
+                System.out.println("Stack Overflow");
+            } else {
+                tos++;
+                data[tos] = val;
+            }
+        }
+
+        int pop(){
+            if(tos == -1){
+                System.out.println("Stack nUnderflow");
+                return -1;
+            } else{
+                 int val = data[tos];
+                 tos--;
+                 return val;
+            }
+        }
+        int top(){
+            if(tos == -1){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else{
+                return data[tos];
+            }
+        }
     }
 }
