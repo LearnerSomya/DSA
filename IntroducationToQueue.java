dd6a3b5f17141f789bd3907b2ab364c61c796f01
LearnerSomya <somyasinghal71001@gmail.com>
2023-12-26 23:42:18 +0530

    Introduction To Stack

---
+++ IntroducationToQueue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class IntroducationToQueue {
+    public static void main (String[] args){
+        //Queue and Dqueue are interfaces where Queue Interface is implementd by Dqueue Interface and ArrayDque in a class,
+        //and then Dqueue Interface is implemented by ArrayDque class 
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(10);
+        System.out.println(que);
+        que.add(20);
+        System.out.println(que);
+        que.add(30);
+        System.out.println(que);
+        que.peek();
+        System.out.println(que);
+
+        System.out.println(que.remove());
+        System.out.println(que);
+
+        System.out.println(que.peek());
+        System.out.println(que);
+    }
+}
